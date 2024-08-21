a5c773e385dd8fc47c2ac8a939a96fb2ce2a8d35
kristopherrr <163081495+kristopherrr@users.noreply.github.com>
2024-08-20 19:47:11 -0600

    Add files via upload

---
+++ Rspecyruby/lib/ruby.rb
@@ -0,0 +1,17 @@
+def intro array
+
+#escribir el código
+array.sum
+end
+
+def nombre nom
+#escribir el código
+return "vacío" if nom.empty?
+"hello #{nom.capitalize}"
+end
+
+def datos? array
+
+#escribir el código
+!array.empty?
+end
